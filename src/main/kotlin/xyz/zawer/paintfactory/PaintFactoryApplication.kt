b30844f79c8f59c8b5fcbeeecabb93582d8a87a3
Cleber Junior <zaca.cj@gmail.com>
2018-03-28 14:20:10 -0300

    initial setup

---
+++ src/main/kotlin/xyz/zawer/paintfactory/PaintFactoryApplication.kt
@@ -0,0 +1,11 @@
+package xyz.zawer.paintfactory
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PaintFactoryApplication
+
+fun main(args: Array<String>) {
+    runApplication<PaintFactoryApplication>(*args)
+}
